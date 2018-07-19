6e0c487c9f18c8e362f13c890df69be2438c99d9
Rob Whitehead <stuff@rjfw.co.uk>
2018-07-19 23:49:10 +0200

    made dylib work

--- main.go
+++ main.go
@@ -1,37 +1,42 @@
 package main
 
 
-import "fmt"
-
 //
-// #cgo CFLAGS: -I./work -v
-// #cgo LDFLAGS: /Users/rjfwhite/go/src/github.com/rjfwhite/helloc/libworker.dylib -v
+// #cgo CFLAGS: -I./work
+// #cgo LDFLAGS: -L./work -lworker
 //
 import "C"
+import "fmt"
 
 func main() {
 //     params := Worker_DefaultConnectionParameters()
 //     connection := Worker_ConnectAsync("hostname", 1337, "myworker", params)
      component := Schema_CreateComponentUpdate(32)
+     fields := Schema_GetComponentUpdateFields(component)
 
-     Schema_AddInt32(component, 1, 32)
-     Schema_AddFloat(component, 2, 5.0)
-     object := Schema_AddObject(component, 3)
-     Schema_AddInt32(object, 1, 1337)
-     data := []byte{1,2,3}
-     Schema_AddBytes(object, 2, &data[0], 3)
+     Schema_AddInt32(fields, 1, 32)
+     val := Schema_GetInt32(fields, 1)
+
+     fmt.Println(val)
+
+
+   //  Schema_AddFloat(component, 2, 5.0)
+    // object := Schema_AddObject(component, 3)
+    // Schema_AddInt32(object, 1, 1337)
+     //data := []byte{1,2,3}
+     //Schema_AddBytes(object, 2, &data[0], 3)
 
 //     op_list := Worker_Connection_GetOpList(connection, 0)
 
      //Worker_OpList_GetOp(op_list, 1)
 
 
-     fields := Schema_GetComponentUpdateFields(component)
-
-     if Schema_GetInt32Count(fields, 1) > 0 {
-          data := Schema_GetInt32(fields, 1)
-          fmt.Print(data)
-     }
+     //fields := Schema_GetComponentUpdateFields(component)
+	 //
+     //if Schema_GetInt32Count(fields, 1) > 0 {
+     //     data := Schema_GetInt32(fields, 1)
+     //     fmt.Print(data)
+     //}
 
 
 
