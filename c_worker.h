8bdd18b9bd67c1b5cbc05279ec21fa9c8067faf0
DESKTOP-8H935GE\rob <rob@improbable.io>
2018-07-21 15:00:24 +0100

    comment out acquire and release

--- c_worker.h
+++ c_worker.h
@@ -373,22 +373,22 @@ typedef struct Worker_ComponentUpdate {
   Worker_ComponentUpdateHandle* user_handle;
 } Worker_ComponentUpdate;
 
-/** Acquire a reference to extend the lifetime of a command request owned by the SDK. */
-Worker_CommandRequest* Worker_AcquireCommandRequest(const Worker_CommandRequest* request);
-/** Acquire a reference to extend the lifetime of a command response owned by the SDK. */
-Worker_CommandResponse* Worker_AcquireCommandResponse(const Worker_CommandResponse* response);
-/** Acquire a reference to extend the lifetime of a component data snapshot owned by the SDK. */
-Worker_ComponentData* Worker_AcquireComponentData(const Worker_ComponentData* data);
-/** Acquire a reference to extend the lifetime of a component update owned by the SDK. */
-Worker_ComponentUpdate* Worker_AcquireComponentUpdate(const Worker_ComponentUpdate* update);
-/** Release a reference obtained by Worker_AcquireCommandRequest. */
-void Worker_ReleaseCommandRequest(Worker_CommandRequest* request);
-/** Release a reference obtained by Worker_AcquireCommandResponse. */
-void Worker_ReleaseCommandResponse(Worker_CommandResponse* response);
-/** Release a reference obtained by Worker_AcquireComponentData. */
-void Worker_ReleaseComponentData(Worker_ComponentData* data);
-/** Release a reference obtained by Worker_AcquireComponentUpdate. */
-void Worker_ReleaseComponentUpdate(Worker_ComponentUpdate* update);
+///** Acquire a reference to extend the lifetime of a command request owned by the SDK. */
+//Worker_CommandRequest* Worker_AcquireCommandRequest(const Worker_CommandRequest* request);
+///** Acquire a reference to extend the lifetime of a command response owned by the SDK. */
+//Worker_CommandResponse* Worker_AcquireCommandResponse(const Worker_CommandResponse* response);
+///** Acquire a reference to extend the lifetime of a component data snapshot owned by the SDK. */
+//Worker_ComponentData* Worker_AcquireComponentData(const Worker_ComponentData* data);
+///** Acquire a reference to extend the lifetime of a component update owned by the SDK. */
+//Worker_ComponentUpdate* Worker_AcquireComponentUpdate(const Worker_ComponentUpdate* update);
+///** Release a reference obtained by Worker_AcquireCommandRequest. */
+//void Worker_ReleaseCommandRequest(Worker_CommandRequest* request);
+///** Release a reference obtained by Worker_AcquireCommandResponse. */
+//void Worker_ReleaseCommandResponse(Worker_CommandResponse* response);
+///** Release a reference obtained by Worker_AcquireComponentData. */
+//void Worker_ReleaseComponentData(Worker_ComponentData* data);
+///** Release a reference obtained by Worker_AcquireComponentUpdate. */
+//void Worker_ReleaseComponentUpdate(Worker_ComponentUpdate* update);
 
 /** Represents an entity with an ID and a component data snapshot. */
 typedef struct Worker_Entity {
